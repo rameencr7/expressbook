1f75ef48e7d49d6ddba2f893fed6f43d36d05094
rameencr7 <136815161+rameencr7@users.noreply.github.com>
2023-12-05 16:30:47 +0400

    Add files via upload

---
+++ expressBookReviews-main/expressBookReviews-main/final_project/router/general.js
@@ -0,0 +1,78 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+public_users.post("/register", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!doesExist(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User " +username+ " successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User " +username+ " already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
+  });
+
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+    res.send(JSON.stringify(books,null,4))
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+    const isbn = req.params.isbn;
+    res.send(books[isbn])
+ });
+  
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+    let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["author"] === req.params.author) {
+        booksbyauthor.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+      }
+    });
+    res.send(JSON.stringify({booksbyauthor}, null, 4));
+});
+
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+    let booksbytitle = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["title"] === req.params.title) {
+        booksbytitle.push({"isbn":isbn,
+                            "author":books[isbn]["author"],
+                            "reviews":books[isbn]["reviews"]});
+      }
+    });
+    res.send(JSON.stringify({booksbytitle}, null, 4));
+});
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+    const isbn = req.params.isbn;
+    res.send(books[isbn]["reviews"])
+});
+
+module.exports.general = public_users;
